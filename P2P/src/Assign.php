f9f38fcc5e4dd19a061ca7132d4e9b95bd91ff82
Usman Irale <superirale@gmail.com>
2016-12-26 13:54:31 +0100

    user registration events listeners added

---
+++ P2P/src/Assign.php
@@ -0,0 +1,16 @@
+<?php
+namespace P2P;
+
+class Assign{
+
+   function _construct($users)
+   {
+   		$this->users = $users;
+   }
+
+   public function selectPair()
+   {
+   		//sort users by date registered oldest first
+   		//
+   }
+}
